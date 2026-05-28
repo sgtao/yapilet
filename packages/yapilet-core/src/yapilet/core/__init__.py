31c73eedcee298dd3dbd5c40e26c099ea799d1fb
Shogo Ogami <sg.tao.so@gmail.com>
2026-05-29 00:47:54 +0900

    fix: remove orphan packages/ dir and spec-deviation dependency

--- packages/yapilet-core/src/yapilet/core/__init__.py
+++
@@ -1,1 +0,0 @@
-"""yapilet core: YAML-driven API request execution."""
--- packages/yapilet-core/src/yapilet/core/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from yapilet.core.models.api_request import ApiRequest
-from yapilet.core.models.result import Result
-
-__all__ = ["ApiRequest", "Result"]
