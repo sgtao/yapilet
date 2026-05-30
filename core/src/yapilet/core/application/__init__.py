5a45dfd1d6b83c4e992c8541b85535de6b01f930
Shogo Ogami <sg.tao.so@gmail.com>
2026-05-30 14:47:54 +0900

    Revert "feat: add ExecuteMessagesUseCase for messages_config YAML"
    This reverts commit 055cffd3f188fb52a6522ccbf7e7a87561562cf5.

--- core/src/yapilet/core/application/__init__.py
+++ core/src/yapilet/core/application/__init__.py
@@ -1,5 +1,4 @@
 from yapilet.core.application.execute_action import ExecuteActionUseCase
-from yapilet.core.application.execute_messages import ExecuteMessagesUseCase
 from yapilet.core.application.execute_single import ExecuteSingleUseCase
 
-__all__ = ["ExecuteActionUseCase", "ExecuteMessagesUseCase", "ExecuteSingleUseCase"]
+__all__ = ["ExecuteActionUseCase", "ExecuteSingleUseCase"]
